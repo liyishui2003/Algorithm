8aae73c120e993481a2826342bee2857d250fa70
Y1shuiLi <98449352+liyishui2003@users.noreply.github.com>
2025-03-11 15:42:42 +0800

    Create 合并两个有序数组.cpp

---
+++ leetcode/合并两个有序数组.cpp
@@ -0,0 +1,25 @@
+/*
+倒序能避免覆盖
+正序也能实现空间为O(m+n)，考虑每次把num1[]中的数往尾巴放就行，维护一个当前最后面到哪里
+但很麻烦，远不如倒叙优雅。
+*/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1, p2 = n - 1;
+        int tail = m + n - 1;
+        int cur;
+        while (p1 >= 0 || p2 >= 0) {
+            if (p1 == -1) {
+                cur = nums2[p2--];
+            } else if (p2 == -1) {
+                cur = nums1[p1--];
+            } else if (nums1[p1] > nums2[p2]) {
+                cur = nums1[p1--];
+            } else {
+                cur = nums2[p2--];
+            }
+            nums1[tail--] = cur;
+        }
+    }
+};

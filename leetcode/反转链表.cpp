4276cdbf798cd1cec5311e975e3323bd1fba6ac6
Y1shuiLi <98449352+liyishui2003@users.noreply.github.com>
2025-03-11 19:50:18 +0800

    Update 反转链表.cpp

--- leetcode/反转链表.cpp
+++ leetcode/反转链表.cpp
@@ -8,17 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* cur = nullptr;
-        ListNode* ret = nullptr;
-        while(head != nullptr){
-            ListNode* tmp = cur;
-            cur = new ListNode(head->val);
-            cur->next = tmp;
-            head=head->next;
+//写法1，改变next
+ListNode* rev(ListNode* head){
+        ListNode* cur = head;
+        if(head==nullptr) return head;
+        ListNode* pre = nullptr;
+        while(1){
+            ListNode* nxt = cur->next;
+            cur->next = pre;
+            pre = cur;
+            if(nxt==nullptr) break;
+            else cur = nxt;
         }
-        return cur;
+        return cur;//这里的cur是头节点
     }
-};
+//写法2，新建节点
+ListNode* reverseList(ListNode* head) {
+    ListNode* cur = nullptr;
+    ListNode* ret = nullptr;
+    while(head != nullptr){
+        ListNode* tmp = cur;
+        cur = new ListNode(head->val);
+        cur->next = tmp;
+        head=head->next;
+    }
+    return cur;
+}
+

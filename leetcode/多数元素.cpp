6e09919b680d2c123f36cc4badf1f079d2bdc198
Y1shuiLi <98449352+liyishui2003@users.noreply.github.com>
2025-03-29 15:49:21 +0800

    Create 多数元素.cpp

---
+++ leetcode/多数元素.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int cnt = 1,most = nums[0];
+        int n = nums.size();
+        for(int i = 1;i < n;i++){
+            if(nums[i] != most){
+                cnt --;
+            }
+            else cnt++;
+            if(cnt == 0){
+                cnt = 1;
+                most = nums[i];
+            }
+        }
+        return most;
+    }
+};

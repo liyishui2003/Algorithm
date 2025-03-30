9a091dc6f3cdc320c2580202c07320e717db86ea
Y1shuiLi <98449352+liyishui2003@users.noreply.github.com>
2025-03-30 17:32:34 +0800

    Create 二叉树的右视图.cpp

---
+++ leetcode/二叉树的右视图.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        
+        vector<int>ret;
+        queue<TreeNode*>q;
+        if(root==nullptr) return ret;
+        q.push(root);
+        while(!q.empty()){
+            deque<int>p;
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                auto v=q.front();
+                q.pop();
+                p.push_back(v->val);
+                if(v->left!=nullptr){
+                    q.push(v->left);
+                }
+                if(v->right!=nullptr){
+                    q.push(v->right);
+                }
+            }
+            ret.push_back(p.back());
+        }
+        return ret;
+ 
+    }
+};
